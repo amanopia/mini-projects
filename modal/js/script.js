3506f4b5f78cce8258627646ca699c7e88554b6e
unknown <amanvermaspace@gmail.com>
2022-12-31 12:02:14 +0530

    added a little text for my understanding

--- modal/js/script.js
+++ modal/js/script.js
@@ -3,6 +3,7 @@ let closeModal = document.querySelector('.close-modal');
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
 
+// For loop is a control structure that will automate any repetitive task, thus it will automate the task of adding eventlistener to each button individually
 for(let i = 0; i<showModalArray.length; i++){
     showModalArray[i].addEventListener('click', function(){
         overlay.classList.remove("hidden");
