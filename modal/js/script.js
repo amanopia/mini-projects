6f7a4ebdc749f2087b3aadd117b56530d1de048a
unknown <amanvermaspace@gmail.com>
2022-12-31 12:28:35 +0530

    added comments to the code. Added functionality to hide the modal when clicked outside the modal. Separated the close modal code into a function

--- modal/js/script.js
+++ modal/js/script.js
@@ -3,16 +3,20 @@ let closeModal = document.querySelector('.close-modal');
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
 
+function hideModal(){
+    modal.classList.add('hidden');
+    overlay.classList.toggle('hidden');
+}
 // For loop is a control structure that will automate any repetitive task, thus it will automate the task of adding eventlistener to each button individually
 for(let i = 0; i<showModalArray.length; i++){
     showModalArray[i].addEventListener('click', function(){
-        overlay.classList.remove("hidden");
         modal.classList.remove('hidden');
+        overlay.classList.toggle("hidden");
     })
 }
 
-closeModal.addEventListener('click', function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-})
+// Click on cross button to close the modal
+closeModal.addEventListener('click', hideModal)
 
+// click outside the modal, on the overlay to close the modal
+overlay.addEventListener('click', hideModal)
