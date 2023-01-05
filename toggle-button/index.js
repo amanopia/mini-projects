cb64726fa513a90b9190962a13652946455ca902
unknown <amanvermaspace@gmail.com>
2023-01-05 09:16:43 +0530

    added dark mdoe to body via toggle

--- toggle-button/index.js
+++ toggle-button/index.js
@@ -1,9 +1,11 @@
 let toggleSwitch = document.querySelector('.toggle-switch');
 let toggleContainer = document.querySelector('.toggle-container');
+let body = document.querySelector('body');
 
 toggleSwitch.addEventListener('click', function(){
     toggleContainer.classList.toggle('green-background');
     toggleSwitch.classList.toggle('sliding-animation');
+    body.classList.toggle('dark-background');
 })
 
 // Chnging element state by adding and removing classes
