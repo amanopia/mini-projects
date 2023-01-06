0dba0bccd551225d3221dee40e2cfa52a790bccb
unknown <amanvermaspace@gmail.com>
2023-01-06 21:40:04 +0530

    Added a new news homepage project

--- toggle-button/index.js
+++ toggle-button/index.js
@@ -8,7 +8,7 @@ toggleSwitch.addEventListener('click', function(){
     body.classList.toggle('dark-background');
 })
 
-// Chnging element state by adding and removing classes
+// Changing element state by adding and removing classes
 .toggleContainer.addEventListener('click', function() {
     toggleContainer.classList.toggle('green-background');
     toggleSwitch.classList.toggle('sliding-animation');
