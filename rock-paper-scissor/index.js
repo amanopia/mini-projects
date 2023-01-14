10936df3ebcbd9178a8e9ac7c3ea6ea1df078f30
unknown <amanvermaspace@gmail.com>
2023-01-14 08:38:41 +0530

    added a random number generating function to select a choice and later compare it with user choice

--- rock-paper-scissor/index.js
+++ rock-paper-scissor/index.js
@@ -1,4 +1,23 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
+'use strict'
+
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
 const result = document.getElementById('result');
-console.log(computerChoice, userChoice, result);
+
+const possibleChoices = document.querySelectorAll('.button');
+console.log(possibleChoices);
+
+let userChoice;
+for(let i = 0; i<possibleChoices.length; i++){
+    // in this function, we are selecting either a rock, paper, or a scissor using a button
+    possibleChoices[i].addEventListener('click', function(event){
+        userChoice = event.target.id;
+        userChoiceDisplay.innerHTML = userChoice;
+    })
+
+}
+
+function generateComputerChoice() {
+    const randomNumber = Math.ceil(Math.random() * 3); // or possibleChoices.length
+    return randomNumber;
+}
