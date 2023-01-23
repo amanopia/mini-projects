a1650629b8049614ceae296003cab3f7ce623021
unknown <amanvermaspace@gmail.com>
2023-01-23 21:14:46 +0530

    finished the toggle switch

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,7 +1,10 @@
 const toggleSwitch = document.querySelector('.toggle-switch');
+const toggleContainer = document.querySelector('.toggle-container');
 const body = document.querySelector('body');
 
 toggleSwitch.addEventListener('click', function(){
     // class to toggle dark mode on body
     body.classList.toggle('dark-mode-color');
+    toggleSwitch.classList.toggle('sliding-animation');
+    toggleContainer.classList.toggle('green-background');
 })
