82ed46929b4b8734d4ced077d313622aecb88257
unknown <amanvermaspace@gmail.com>
2023-01-24 13:40:48 +0530

    added new images and added code for intersection observer

--- algorithms/index.js
+++ algorithms/index.js
@@ -2,6 +2,22 @@ const toggleSwitch = document.querySelector('.toggle-switch');
 const toggleContainer = document.querySelector('.toggle-container');
 const body = document.querySelector('body');
 
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+        console.log(entry);
+
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 toggleSwitch.addEventListener('click', function(){
     // class to toggle dark mode on body
     body.classList.toggle('dark-mode-color');
